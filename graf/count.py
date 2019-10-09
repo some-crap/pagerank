af6832303c1b4d38c16896c477569971bfaa9383
Yury Durov <43110604+some-crap@users.noreply.github.com>
2019-10-09 22:18:13 +0300

    Add files via upload

---
+++ graf/count.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# Для запуска программы необходимо скачать интерпретатор языка Python3.7 и установить его
+# https://www.python.org/ftp/python/3.7.4/python-3.7.4.exe
+# После этого необходимо иметь активное соединение с интернетом, 
+# открыть командную строку от имени админиматратора и написать следущие комманды:
+# pip install numpy
+# pip install matplotlib
+# pip install networkx
+# После этого можно запускать программу
+# На вход подаётся файл CSV (Первая строка и первый столбец - заголовки)
+# На выход выдаётся список, ранжированный по алгоритму PageRank
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+def vector_len(arr):
+    l = arr.shape[0]
+    res = 0
+    for i in range(l):
+        res = res + (arr[i])**2
+    res = res ** 0.5
+    return res
+
+
+def matrix_multiplication_by_vector(matrix, vector):
+    l = matrix.shape[0]
+    res = np.zeros(matrix.shape[0])
+    for i in range(l):
+        for n in range(matrix.shape[1]):
+            res[i] = res[i]+matrix[i, n] * vector[n]
+    return res
+
+
+def matrix_gen(non_stochastic_matrix,N):
+    res = non_stochastic_matrix
+    for i in range(N-1):
+        tmp = 0
+        for n in range(N-1):
+            tmp += res[n, i]
+        if tmp != 0:
+            tmp = 1 / tmp
+        for n in range(N-1):
+            res[n, i] = tmp * res[n, i]
+    return res
+
+
+def calculate_pages(matrix, vector, depth=1e-6):
+    temp = 2
+    count = 0
+    while temp > depth:
+        x1 = matrix_multiplication_by_vector(matrix, vector)
+        x1 = x1 / np.sum(np.abs(x1))
+        temp = vector_len(x1 - vector)
+        #print(temp)
+        vector = x1
+        count += 1
+    #print(np.sum(x1))
+    print(count)
+    return x1
+
+
+datafile = open('C:\\Users\\yuram\\Desktop\\pagerank\\123.csv', 'r')
+# reading CSV
+lines = datafile.read().split(sep='\n')
+N = len(lines)
+lms = lines[0].split(sep=';')
+DATA = np.zeros([N-1, N-1])
+names = []
+for i in range(N-1):
+    elms = lines[i].split(sep=';')
+    print(i)
+    names.append(elms[0])
+    for j in range(N-1):
+        DATA[i, j] = float(elms[j+1])
+# reading CSV
+size = N-1
+M = matrix_gen(DATA,N)
+G = nx.from_numpy_matrix(DATA)
+nx.draw_networkx(G)
+plt.show()
+x0 = np.ones(size)/size
+eps = 1e-5
+E = np.ones([size,size])
+d = 0.0005 # https://en.wikipedia.org/wiki/PageRank#Power_Method (if we have hanging nodes)
+E = (E * d)/size
+M = (1 - d) * M + E
+x1 = calculate_pages(M, x0, eps)
+all = []
+for i in range(N-1):
+    a = []
+    a.append(x1[i])
+    a.append(names[i])
+    all.append(a)
+all = sorted(all, reverse=True)
+strin = ""
+for i in range(N-1):
+    print( str(i+1)+") "+all[i][1]+": "+str(round((all[i][0]*100), 2))+"%")
+input()
